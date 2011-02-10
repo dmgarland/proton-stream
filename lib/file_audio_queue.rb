95a25bd86f5387a4e531e2c81886070f3808a128
dmgarland <dan@dangarland.co.uk>
2011-02-10 17:08:02 +0000

    Buffer now wraps around

--- lib/file_audio_queue.rb
+++ lib/file_audio_queue.rb
@@ -8,26 +8,34 @@ module ProtonStream
     
     attr_accessor :buffer_file
     
-    MAX_BUFFER_SIZE = 10240
     BLOCK_SIZE = 1024 * 96
+    MAX_BUFFER_SIZE = 10 * BLOCK_SIZE
     
     # Adds a periodic timer to the Eventmachine reactor loop and starts 
     # appending bytes to the audio queue
     #
     def initialize
-     
+      
       STDOUT.puts "Initialising queue"
       
       self.buffer_file = File.new("/tmp/buffer", "w+")
+      @@current_track = Track.next_track
+      @@already_read = 0
+      
+      @@head = 0
+      @@tail = 0
       
       append_queue
-      @@head = 0
-      #rewind
       
-      EM.add_periodic_timer(2) do
+      
+      
+      # Periodically append more music to the queue      
+      EM.add_periodic_timer(5) do
         append_queue
       end
       
+      # Every second, read a chunk from the head of the queue
+      # so that listening clients can get at it and share the same data
       EM.add_periodic_timer(1) do
         read_chunk
       end
@@ -36,62 +44,82 @@ module ProtonStream
     # Gets the next track from Mongo and appends its data to the buffer.
     #
     def append_queue
-      if File.size(buffer_file) < MAX_BUFFER_SIZE
-        Track.read_next_track(self.buffer_file)
+      if free_space > 0
+        puts "reading #{@@current_track} into buffer"
+        @db = Mongo::Connection.new.db("mostrated")
+        @fs = Mongo::GridFileSystem.new(@db) 
+        
+        track = @fs.open(@@current_track, "r")     
+        
+        if track.file_length > free_space
+          # The file is too big to read into the buffer, so read up until
+          # the free space is used
+          STDOUT.puts "#{free_blocks} free blocks"
+          free_blocks.times {
+            STDOUT.puts "Writing #{BLOCK_SIZE} from track pos #{track.tell} to buffer pos #{@@tail}"
+            if @@already_read < track.file_length
+              track.seek @@already_read
+              buffer_file.seek @@tail 
+              begin
+                bytes = track.read(BLOCK_SIZE)
+              rescue Exception => e
+                STDOUT.puts "fuckup : #{e}"
+              end
+                
+              buffer_file.write bytes     
+              
+              # If we've reached the end of the buffer, wrap around to the front
+              if buffer_file.tell >= MAX_BUFFER_SIZE
+                @@tail = 0
+              else
+                @@tail += BLOCK_SIZE
+              end
+              
+              @@already_read += BLOCK_SIZE
+            else
+              # We've read until until the end of a track, time for another one
+              @@current_track = Track.next_track
+              @@already_read = 0
+            end
+          }
+        else
+          # We can completely read the file into the buffer
+          buffer_file << track.read
+          @@already_read = 0
+        end
+        
+        
+        puts "buffer size: #{File.size(buffer_file)} / #{free_space} free"
+        
       end
     end
     
     def read_chunk
       STDOUT.puts("Reading #{BLOCK_SIZE} bytes from offset #{@@head}")
-#      @@last_chunk = buffer_file.read(BLOCK_SIZE, @@head)
+      
+      # Read a block relative to the head pointer offset
       @@last_chunk = File.read(buffer_file.path, BLOCK_SIZE, @@head)
       @@head += BLOCK_SIZE
+      
+      # If we've read to the end, loop around to the start
       if @@head > File.size(buffer_file)
         @@head = 0
-      end
-        
-        # else
-          # STDOUT.puts "Reached EOF"
-        # end
-      # rescue IOError => e
-        # STDOUT.puts e
-        # STDOUT.puts "pos = #{pos}"
-        # EM.stop
-      # end
-      # if @@last_chunk
-        # STDOUT.puts "read #{@@last_chunk.size} bytes #{@@head}"
-      # else
-        # STDOUT.puts "no bytes on read head = #{@@head} pos = #{pos}"
-      # end
-        
+      end        
     end
     
     def each
-      
-      # timer = EventMachine::PeriodicTimer.new(1) do
-      # bytes = read(BLOCK_SIZE)
-      # 
-      # unless bytes.nil? or bytes.size == 0
-      # yield bytes
-      # STDOUT.puts "Wrote #{bytes.size} bytes  pos=#{pos}"
-      # else
-      # STDOUT.puts "no bytes pos=#{pos}"
-      # end
-      # 
-      # end
-      # errback { timer.cancel }
-      
+      # The callback procs will use the value of p when they are defined      
       p = @@head
       
       stream_writer = proc do 
         STDOUT.puts "stream writer #{p}"
         
-        # wait until the other timer has updated the bytes
+        # wait until the another timer has updated the bytes and moved the head
         until(@@head != p) do
           sleep 0.5
-          #STDOUT.puts "head #{@@head} != #{p}"
         end
         
+        # All the listeners get the same data
         bytes = @@last_chunk
         
         unless bytes.nil? or bytes.size == 0
@@ -104,7 +132,8 @@ module ProtonStream
       end
       
       end_of_write_callback = proc do
-        STDOUT.puts "end of write callbcak"
+        #STDOUT.puts "end of write callbcak"
+        # Update the current position
         p = @@head
         EM.defer(stream_writer, end_of_write_callback)
       end
@@ -112,6 +141,34 @@ module ProtonStream
       EM.defer(stream_writer, end_of_write_callback)
     end
     
+    
+    
+    def free_space
+      buffer_file_size = File.size(buffer_file)
+      free_space = 0
+      
+      if buffer_file_size < MAX_BUFFER_SIZE
+        # We haven't yet filled the buffer
+        free_space = MAX_BUFFER_SIZE - buffer_file_size
+      else
+        # The free space is the difference between the tail and the head
+        
+        if @@head > @@tail
+          free_space = @@head - @@tail
+        elsif @@head == @@tail
+          free_space = 0
+        else
+          free_space = (MAX_BUFFER_SIZE - @@tail) + @@head
+        end
+      end
+      
+      STDOUT.puts "free space = #{free_space} tail = #{@@tail} head = #{@@head}"
+      return free_space
+    end
+    
+    def free_blocks
+      free_space / BLOCK_SIZE
+    end
   end
   
 end
--- lib/proton-stream/models/track.rb
+++ lib/proton-stream/models/track.rb
@@ -1,15 +1,22 @@
 class Track < ActiveRecord::Base
   
-  def self.read_next_track(buffer)
-    puts "reading into buffer"
-    @db = Mongo::Connection.new.db("mostrated")
-    @fs = Mongo::GridFileSystem.new(@db)  
-        
-    track = @fs.open("mindfield.mp3", "r") { |f|
-      buffer << f.read      
-    }
+  #attr_accessor :bytes_read
+  
+  def self.next_track
+     
     
-    puts "buffer size: #{File.size(buffer)}"
+    # Track selection will be more compilcated than this...
+    file = ""
+    if rand(2) == 0
+      file = "/home/daniel/Music/grade 8/PF_G8_A1_J_S_Bach_Capriccio.mp3"      
+    else
+      file = "mindfield.mp3"
+    end
+    
+    STDOUT.puts file
+    return file
+    
+    
   end
   
 end
